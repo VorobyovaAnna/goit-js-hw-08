fd1a72b3e03fffb30054c5b65c100d8c710e7911
VorobyovaAnna <anyasonn1k@gmail.com>
2021-11-29 19:39:43 +0200

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryList.addEventListener('click', onGalleryListClick);
+
+function createGalleryMarkup(items) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join('');
+};
+
+function onGalleryListClick(evt) {
+
+    evt.preventDefault();
+
+    const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryImageEl) {
+        return;
+    };
+    
+    let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+};
