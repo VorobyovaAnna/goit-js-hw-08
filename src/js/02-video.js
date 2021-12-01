31c3917af6d2915f5952c0fe6b7ab373010f0fd3
VorobyovaAnna <anyasonn1k@gmail.com>
2021-12-01 23:28:43 +0200

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY_VIDEOPLAYER = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onTimeUpDate(data) {
+    localStorage.setItem(STORAGE_KEY_VIDEOPLAYER, data.seconds);
+};
+
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY_VIDEOPLAYER);
+    if (currentTime) {
+        player.setCurrentTime(currentTime);
+    }
+    
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
